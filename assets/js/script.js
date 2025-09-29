0ee1c20e3d26259554c3777a2108afbeadc03b71
Aditi Singh <123548974+AditiSingh1208@users.noreply.github.com>
2025-09-29 17:33:12 +0000

    uploaded image

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,27 +105,31 @@ function showSkills(skills) {
 }
 
 function showProjects(projects) {
-    let projectsContainer = document.querySelector("#work .box-container");
-    let projectHTML = "";
-    projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
-        projectHTML += `
-        <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+    let projectsContainer = document.querySelector(".work .box-container");
+    let projectsHTML = "";
+
+    projects.forEach(project => {
+        projectsHTML += `
+        <div class="grid-item ${project.category || ''}">
+          <div class="box tilt" style="width: 380px; margin: 1rem">
+            <img draggable="false" src="${project.image}" alt="${project.name}" />
+            <div class="content">
+              <div class="tag">
+                <h3>${project.name}</h3>
+              </div>
+              <div class="desc">
+                <p>${project.desc}</p>
+                <div class="btns">
+                  ${project.links.view ? `<a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>` : ""}
+                  <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>`
+        </div>`;
     });
-    projectsContainer.innerHTML = projectHTML;
+
+    projectsContainer.innerHTML = projectsHTML;
 
     // <!-- tilt js effect starts -->
     VanillaTilt.init(document.querySelectorAll(".tilt"), {
